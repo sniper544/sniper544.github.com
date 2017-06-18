d9e19388a8f6bb17e77b90a4e948225b8ff1eb93
sniper54 <bogdan.laza2011@yandex.ru>
2017-06-18 15:57:43 +0300

    First commit

---
+++ js/common.js
@@ -0,0 +1,76 @@
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+
+	$(".typing").typed({
+		strings: ["I'm a beginner web-developer.", "From Ukraine"],
+		typeSpeed: 70
+	});
+
+	$(window).on('scroll', function () {
+		if($(window).scrollTop() > 50) {
+			$('.topline-header').addClass('header-fixed');
+		} else {
+			$('.topline-header').removeClass('header-fixed');
+		}
+	});
+
+
+	var $root = $('html, body');
+	$('a').click(function() {
+		var href = $.attr(this, 'href');
+		$root.animate({
+			scrollTop: $(href).offset().top - 80
+		}, 500, function () {
+			window.location.hash = href;
+		});
+		return false;
+	});
+	
+
+});
+
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
